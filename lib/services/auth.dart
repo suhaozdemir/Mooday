abf54501c0c370f20f2f4c50c66f3179fb90c1ab
suhaozdemir <77856467+suhaozdemir@users.noreply.github.com>
2021-11-27 21:53:32 +0300

    Created auth.dart

---
+++ lib/services/auth.dart
@@ -0,0 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Auth {
+  //Add authentication service here use provider if you understand it well.
+}
