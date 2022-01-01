cf0a1dd708955a0402c19e8bd0b20756f6228811
suhaozdemir <77856467+suhaozdemir@users.noreply.github.com>
2022-01-02 00:05:10 +0300

    Edited Timer

--- lib/screens/timer/timer_screen.dart
+++ lib/screens/timer/timer_screen.dart
@@ -1,6 +1,7 @@
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:mooday/screens/timer/localwidgets/circular_timer.dart';
+import 'package:circular_menu/circular_menu.dart';
 
 class TimerScreen extends StatefulWidget {
   @override
@@ -9,31 +10,52 @@ class TimerScreen extends StatefulWidget {
 
 class _TimerScreenState extends State<TimerScreen> {
   CountDownController _controller = CountDownController();
-  bool _isPause = true;
-
+  late int duration = 10;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       backgroundColor: Colors.white,
       body: Center(
           child: CircularTimer(
         controller: _controller,
-        duration: 30,
+        duration: duration,
       )),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          setState(() {
-            if (_isPause) {
-              _isPause = false;
-              _controller.resume();
-            } else {
-              _isPause = true;
-              _controller.pause();
-            }
-          });
-        },
-        icon: Icon(_isPause ? Icons.play_arrow : Icons.pause),
-        label: Text(_isPause ? 'Start' : 'Pause'),
+      floatingActionButton: CircularMenu(
+        toggleButtonColor: Colors.black,
+        items: [
+          CircularMenuItem(
+              color: Color(0xffbfc0cd),
+              iconColor: Colors.black,
+              icon: Icons.stop,
+              onTap: () {
+                setState(() {
+                  duration = 0;
+                  _controller.pause();
+                });
+              }),
+          CircularMenuItem(
+              color: Color(0xffbfc0cd),
+              iconColor: Colors.black,
+              icon: Icons.play_arrow,
+              onTap: () {
+                _controller.resume();
+              }),
+          CircularMenuItem(
+              color: Color(0xffbfc0cd),
+              iconColor: Colors.black,
+              icon: Icons.pause,
+              onTap: () {
+                _controller.pause();
+              }),
+          CircularMenuItem(
+              color: Color(0xffbfc0cd),
+              iconColor: Colors.black,
+              icon: Icons.restart_alt,
+              onTap: () {
+                _controller.restart();
+              }),
+        ],
       ),
     );
   }
